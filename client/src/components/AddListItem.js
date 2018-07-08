935f73e84b48a8dfa21455f26f5d29c38d9b0c79
Annika <liebste.anni@gmail.com>
2018-07-08 21:04:25 +1200

    adds margin for submit

--- client/src/components/AddListItem.js
+++ client/src/components/AddListItem.js
@@ -47,21 +47,23 @@ class NewListForm extends Component {
             <p>Add new Item</p>
             <button className="btn-outline-dark btn-sm" onClick={this.addingItem.bind(this)}><FontAwesomeIcon icon={faPlus} /></button>
           </div>
-        : <form onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <label>Title</label>
-              <input className="form-control" name="title" type="text" placeholder="Title..." onChange={this.handleChange} />
-            </div>
-            <div className="form-group">
-              <label>Excerpt</label>  
-              <input className="form-control" name="excerpt" type="text" placeholder="Excerpt..." onChange={this.handleChange} />
-            </div>  
-            <div>
-              <label>Full description</label>  
-              <textarea className="form-control" rows="10" name="description" type="text" placeholder="Full description..." onChange={this.handleChange} />
-            </div>
-            <button className="btn-outline-dark btn-sm">Submit</button>
-          </form>}
+        : <div className="addForm">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label>Title</label>
+                <input className="form-control" name="title" type="text" placeholder="Title..." onChange={this.handleChange} />
+              </div>
+              <div className="form-group">
+                <label>Excerpt</label>  
+                <input className="form-control" name="excerpt" type="text" placeholder="Excerpt..." onChange={this.handleChange} />
+              </div>  
+              <div>
+                <label>Full description</label>  
+                <textarea className="form-control" rows="10" name="description" type="text" placeholder="Full description..." onChange={this.handleChange} />
+              </div>
+              <button className="btn-outline-dark btn-sm">Submit</button>
+            </form>
+          </div>}
       </div>
     )
   }
